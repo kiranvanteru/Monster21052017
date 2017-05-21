1b6b6a6b527b7272a8aba468b7683df823d85b9d
kiranvanteru <kk23vanteru@gmail.com>
2017-05-21 17:29:00 +0530

    This is Monster 21052017

---
+++ Monster_Job/src/test/java/login/LoginTest.java
@@ -0,0 +1,21 @@
+package login;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class LoginTest {
+  @Test
+  public void f() {
+	  System.out.println("monster");
+	  
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
